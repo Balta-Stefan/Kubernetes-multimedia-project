46f89e6ef079ac5e336d5f516e0364bdeedfe525
Balta-Stefan <pivad@protonmail.ch>
2022-06-05 15:48:46 +0200

    Removed @EnableScheduling annotation from WorkerApplication.

--- backend/worker/src/main/java/pisio/worker/WorkerApplication.java
+++ backend/worker/src/main/java/pisio/worker/WorkerApplication.java
@@ -2,10 +2,11 @@ package pisio.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"pisio.common", "pisio.worker"})
-@EnableScheduling
+@EnableKafka
 public class WorkerApplication
 {
     public static void main(String[] args)
