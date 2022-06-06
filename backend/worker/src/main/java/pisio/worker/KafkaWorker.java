1e8aab50fd41f471e34b675924e5e5262bb10d1b
Balta-Stefan <pivad@protonmail.ch>
2022-06-06 14:02:47 +0200

    KafkaWorker in worker module finished.Testing not performed.

--- backend/worker/src/main/java/pisio/worker/KafkaWorker.java
+++ backend/worker/src/main/java/pisio/worker/KafkaWorker.java
@@ -1,5 +1,8 @@
 package pisio.worker;
 
+import io.minio.DownloadObjectArgs;
+import io.minio.MinioClient;
+import io.minio.UploadObjectArgs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaHandler;
@@ -10,8 +13,11 @@ import pisio.common.model.enums.ProcessingProgress;
 import pisio.common.model.messages.BaseMessage;
 import pisio.common.model.messages.ExtractAudioMessage;
 import pisio.common.model.messages.Transcode;
-import pisio.worker.services.AudioService;
 import pisio.worker.services.VideoService;
+
+import java.io.File;
+import java.util.Optional;
+import java.util.UUID;
 
 @KafkaListener(id = "${pending-topic-group-id}", topics = "${pending.topic-name}")
 @Slf4j
@@ -21,52 +27,111 @@ public class KafkaWorker
     private final KafkaTemplate<String, BaseMessage> kafkaTemplate;
     @Value("${finished.topic-name}")
     private String finishedTopicName;
-    private final AudioService audioService;
     private final VideoService videoService;
 
-    public KafkaWorker(KafkaTemplate<String, BaseMessage> kafkaTemplate, AudioService audioService, VideoService videoService)
+    private final MinioClient minioClient;
+
+    public KafkaWorker(KafkaTemplate<String, BaseMessage> kafkaTemplate, VideoService videoService, MinioClient minioClient)
     {
         this.kafkaTemplate = kafkaTemplate;
-        this.audioService = audioService;
         this.videoService = videoService;
+        this.minioClient = minioClient;
+    }
+
+    private Optional<String> downloadFile(String bucket, String object)
+    {
+        String fileName = UUID.randomUUID().toString();
+
+        try
+        {
+            minioClient.downloadObject(
+                    DownloadObjectArgs.builder()
+                            .bucket(bucket)
+                            .object(object)
+                            .filename(fileName)
+                            .build());
+        }
+        catch(Exception e)
+        {
+            log.warn("An exception has occured in KafkaWorker.downloadFile");
+            e.printStackTrace();
+            return Optional.of(fileName);
+        }
+
+        return Optional.empty();
+    }
+
+    private boolean uploadFile(String bucket, String fileName, String filePath)
+    {
+        try
+        {
+            minioClient.uploadObject(
+                    UploadObjectArgs.builder()
+                            .bucket(bucket).object("finished/" + fileName)
+                            .filename(filePath)
+                            .build());
+        }
+        catch(Exception e)
+        {
+            log.warn("Failed to upload a finished file");
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+    private void handleProcessedFile(BaseMessage msg, Optional<String> outputFilePathOpt)
+    {
+        BaseMessage response = new BaseMessage(msg);
+        response.setProgress(ProcessingProgress.FAILED);
+
+        String outputFilePath;
+        if(outputFilePathOpt.isEmpty())
+        {
+            kafkaTemplate.send(finishedTopicName, response);
+            return;
+        }
+        else
+        {
+            outputFilePath = outputFilePathOpt.get();
+        }
+
+        if(uploadFile(msg.getBucket(), msg.getFileName(), outputFilePath) == true)
+        {
+            response.setProgress(ProcessingProgress.FINISHED);
+
+            File file = new File(outputFilePath);
+            if(file.delete() == false)
+            {
+                log.warn("Couldn't delete processed file: " + outputFilePath);
+            }
+        }
+        else
+        {
+            log.warn("File not uploaded successfully.");
+        }
+
+        kafkaTemplate.send(finishedTopicName, response);
     }
 
     @KafkaHandler
     public void extractAudio(ExtractAudioMessage msg)
     {
-        log.warn("Worker has received an extract audio message: " + msg.toString());
-        boolean status = audioService.extractAudio(msg);
-
-        BaseMessage response = new BaseMessage(msg);
-
-        if(status == true)
+        downloadFile(msg.getBucket(), msg.getObject()).ifPresent(downloadedFilePath ->
         {
-            response.setProgress(ProcessingProgress.FINISHED);
-        }
-        else
-        {
-            response.setProgress(ProcessingProgress.FAILED);
-        }
-        kafkaTemplate.send(finishedTopicName, response);
+            handleProcessedFile(msg, videoService.extractAudio(downloadedFilePath));
+        });
     }
 
     @KafkaHandler
     public void transcodeVideo(Transcode msg)
     {
-        log.info("Worker has received a transcode message: " + msg.toString());
-        boolean status = videoService.transcode(msg);
-
-        BaseMessage response = new BaseMessage(msg);
-
-        if(status == true)
+        downloadFile(msg.getBucket(), msg.getObject()).ifPresent(downloadedFilePath ->
         {
-            response.setProgress(ProcessingProgress.FINISHED);
-        }
-        else
-        {
-            response.setProgress(ProcessingProgress.FAILED);
-        }
-
-        kafkaTemplate.send(finishedTopicName, response);
+            handleProcessedFile(msg, videoService.transcode(downloadedFilePath, msg.getTargetResolution()));
+        });
     }
 }
