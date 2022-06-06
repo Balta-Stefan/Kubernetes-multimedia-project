08d42e2668d282bdd834864be3b63b2cde8fe6eb
Balta-Stefan <pivad@protonmail.ch>
2022-06-06 14:03:44 +0200

    WebSocket configuration complete.User destinations are not working.

--- backend/backend_service/src/main/java/pisio/backend/configuration/WebSocketConfiguration.java
+++ backend/backend_service/src/main/java/pisio/backend/configuration/WebSocketConfiguration.java
@@ -12,27 +12,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer
 {
-    private final TaskScheduler messageBrokerScheduler;
-
-    public WebSocketConfiguration(@Lazy TaskScheduler messageBrokerScheduler)
-    {
-        this.messageBrokerScheduler = messageBrokerScheduler;
-    }
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry)
     {
-        registry.addEndpoint("/notifications").withSockJS(); // the endpoint is /ws/notifications
+        registry.addEndpoint("/register").setAllowedOrigins("*");
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry)
     {
         // destinations prefixed with /user target a message queue for the user that made that request
-        registry.enableSimpleBroker("/user/queue/notifications")
-                .setHeartbeatValue(new long[]{10000, 20000}) // server sends a heartbeat every 10 seconds while client has to send a heartbeat every 20 seconds
-                .setTaskScheduler(this.messageBrokerScheduler); // documentation is vague.If a message contains this URI, it is probably sent directly to the in memory broker and not to the @Controller
+        registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/ws"); // messages whose URL starts with this prefix are routed to the @Controller
-        registry.setUserDestinationPrefix("/user");
     }
 }
