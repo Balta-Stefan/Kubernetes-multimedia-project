c0eb3f35b8683e6c0fa35ef2926ca3baeef2019d
Balta-Stefan <pivad@protonmail.ch>
2022-06-06 12:51:18 +0200

    WorkerApplication now has MinioClient bean.

--- backend/worker/src/main/java/pisio/worker/WorkerApplication.java
+++ backend/worker/src/main/java/pisio/worker/WorkerApplication.java
@@ -1,7 +1,10 @@
 package pisio.worker;
 
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -9,8 +12,29 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableKafka
 public class WorkerApplication
 {
+    @Value("${minio.endpoint}")
+    private String minioEndpoint;
+
+    @Value("${minio.port}")
+    private int minioEndpointPort;
+
+    @Value("${minio.access_key}")
+    private String accessKey;
+
+    @Value("${minio.secret_access_key}")
+    private String secret;
+
     public static void main(String[] args)
     {
         SpringApplication.run(WorkerApplication.class, args);
+    }
+
+    @Bean
+    public MinioClient minioClient()
+    {
+        return MinioClient.builder()
+                .endpoint(minioEndpoint, minioEndpointPort, false)
+                .credentials(accessKey, secret)
+                .build();
     }
 }
