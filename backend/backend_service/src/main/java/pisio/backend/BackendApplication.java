1703b53f5d7abc3c4155d451af5c0c35e783ee8c
Balta-Stefan <pivad@protonmail.ch>
2022-06-08 16:21:42 +0200

    BackendApplication - added EnableKafka annotation.

--- backend/backend_service/src/main/java/pisio/backend/BackendApplication.java
+++ backend/backend_service/src/main/java/pisio/backend/BackendApplication.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication(scanBasePackages = {"pisio.common", "pisio.backend"})
+@EnableKafka
 public class BackendApplication
 {
     @Value("${minio.endpoint}")
