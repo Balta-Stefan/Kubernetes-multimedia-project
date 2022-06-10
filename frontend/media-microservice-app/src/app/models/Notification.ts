c5ca6ee26622a9cbd005694e63a1373b2dbcbd50
Balta-Stefan <pivad@protonmail.ch>
2022-06-10 13:53:04 +0200

    Notification interface has new field - processingID;

--- frontend/media-microservice-app/src/app/models/Notification.ts
+++ frontend/media-microservice-app/src/app/models/Notification.ts
@@ -2,6 +2,7 @@ import { ProcessingProgress } from "./ProcessingProgress";
 import { ProcessingType } from "./ProcessingType";
 
 export interface Notification{
+    processingID: string | null;
     fileName: string;
     progress: ProcessingProgress | null;
     url: string | null;
