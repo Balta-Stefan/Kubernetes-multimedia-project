19d60b8298c0cf0c14ffd751e0df6a690c416e47
Balta-Stefan <pivad@protonmail.ch>
2022-06-10 13:56:34 +0200

    UserNotification now contains proecssingID and Nullable annotations.

--- backend/common/src/main/java/pisio/common/model/DTOs/UserNotification.java
+++ backend/common/src/main/java/pisio/common/model/DTOs/UserNotification.java
@@ -3,6 +3,7 @@ package pisio.common.model.DTOs;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 import pisio.common.model.enums.ProcessingProgress;
 import pisio.common.model.enums.ProcessingType;
 
@@ -11,8 +12,11 @@ import pisio.common.model.enums.ProcessingType;
 @NoArgsConstructor
 public class UserNotification
 {
+    @Nullable
+    private String processingID;
     private String fileName;
     private ProcessingProgress progress;
+    @Nullable
     private String url;
     private ProcessingType type;
 }
