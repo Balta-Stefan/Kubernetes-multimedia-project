8fb6cf1b3f11ed4a53693339d0472ea789566e1f
Balta-Stefan <pivad@protonmail.ch>
2022-06-10 17:13:18 +0200

    Added LoginReply class to return user's message queue ID.

---
+++ backend/backend_service/src/main/java/pisio/backend/models/LoginReply.java
@@ -0,0 +1,13 @@
+package pisio.backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginReply
+{
+    private String messageQueueID;
+}
